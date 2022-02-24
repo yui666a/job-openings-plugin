8a286f2081ca1bc6430102190ea2267efb45125f
yui666a <haiso666@gmail.com>
2022-02-24 15:49:46 +0900

    add js css

---
+++ job_opening_plugin/js/main.js
@@ -0,0 +1,51 @@
+const position = [
+  { id: 0, text: "契約社員" },
+  { id: 1, text: "正社員" },
+  { id: 2, text: "課長" },
+  { id: 3, text: "エンジニア" },
+];
+
+$(function () {
+  const select = $("#position");
+  select.select2({
+    placeholder: "選択してください",
+    data: position,
+  });
+  select.val().trigger("change");
+  // .on('change', (event) => {
+  //   const selecions = select.select2('data').map((element) => parseInt(element.id, 10));
+  // });
+});
+
+const locations = [
+  { id: 0, text: "長岡" },
+  { id: 1, text: "大阪" },
+  { id: 2, text: "渋田" },
+];
+
+$(function () {
+  const select = $("#location");
+  select.select2({
+    placeholder: "選択してください",
+    data: locations,
+  });
+  select.val([0, 1]).trigger("change");
+  // .on('change', (event) => {
+  //   const selecions = select.select2('data').map((element) => parseInt(element.id, 10));
+  // });
+});
+
+const occupations = [
+  { id: 0, text: "Webデザイナー" },
+  { id: 1, text: "フロントエンジニア" },
+  { id: 2, text: "インフラエンジニア" },
+];
+
+$(function () {
+  const select = $("#occupation");
+  select.select2({
+    placeholder: "選択してください",
+    data: occupations,
+  });
+  select.val([]).trigger("change");
+});
