89ff432d833c8efca65ee9229a4ec85ead90894f
yui666a <haiso666@gmail.com>
2022-02-25 16:44:42 +0900

    投稿一覧表示

--- job_opening_plugin/view/template/jobTable.php
+++ job_opening_plugin/view/template/jobTable.php
@@ -1,44 +1,52 @@
 <?php
+function console_log($data)
+{
+  echo '<script>';
+  echo 'console.log(' . json_encode($data) . ')';
+  echo '</script>';
+}
 
-function make_job_openings_table_row($title) {
+function make_job_openings_table_row($post_id, $title, $author, $post_date, $job_expires, $job_location, $status_icon)
+{
+  $admin_url = esc_url(get_admin_url(''));
   $job_openings_table_main = '
     <tr
-      id="post-11"
-      class="iedit author-self level-0 post-11 type-job_listing status-publish has-post-thumbnail hentry job_listing job-type-full-time"
+      id="post-' . $post_id . '"
+      class="iedit author-self level-0 post-' . $post_id . ' type-job_listing status-publish has-post-thumbnail hentry job_listing job-type-full-time"
     >
       <th scope="row" class="check-column">
-        <label class="screen-reader-text" for="cb-select-11">' . '$title' . '
+        <label class="screen-reader-text" for="cb-select-' . $post_id . '">' . $title . '
         </label>
-        <input id="cb-select-11" type="checkbox" name="post[]" value="11" />
+        <input id="cb-select-' . $post_id . '" type="checkbox" name="post[]" value="' . $post_id . '" />
         <div class="locked-indicator">
           <span class="locked-indicator-icon" aria-hidden="true"></span>
           <span class="screen-reader-text">
-            “' . '$title' . '” はロックされています
+            “' . $title . '” はロックされています
           </span>
         </div>
       </th>
       <td
         class="job_position column-job_position has-row-actions column-primary"
-        data-colname="位置"
+        data-colname="ポジション"
       >
         <div class="job_position">
           <a
-            href="http://aisohitoshi.wp.xdomain.jp/wp-admin/post.php?post=11&amp;action=edit"
+            href="' . $admin_url . 'post.php?post=' . $post_id . '&amp;action=edit"
             class="tips job_title"
-            data-tip="ID: 11"
-            >' . '$title' . '</a
+            data-tip="ID: ' . $post_id . '"
+            >' . $title . '</a
           >
           <div class="company">
             <span class="tips" data-tip="asdf"
-              ><a href="https://yui666a.github.io/home/pc.html"
-                >アンタッチャブル</a
+              ><a href="https://yui666a.github.io/home/pc.html(TODO)"
+                >' . $author . '</a
               ></span
             >
           </div>
           <img
             class="company_logo"
-            src="http://aisohitoshi.wp.xdomain.jp/wp-content/uploads/job-manager-uploads/company_logo/2022/02/SAIN_logo-150x150.png"
-            alt="アンタッチャブル"
+            src="/wp-content/uploads/job-manager-uploads/company_logo/2022/02/SAIN_logo-150x150.png"
+            alt="' . $author . '"
           />
         </div>
         <button type="button" class="toggle-row">
@@ -54,15 +62,15 @@ function make_job_openings_table_row($title) {
       <td class="job_location column-job_location" data-colname="所在地">
         <a
           class="google_map_link"
-          href="https://maps.google.com/maps?q=%E5%9C%B0%E7%90%83&amp;zoom=14&amp;size=512x512&amp;maptype=roadmap&amp;sensor=false"
-          >地球</a
+          href="https://maps.google.com/maps?q=' . $job_location . '&amp;zoom=14&amp;size=512x512&amp;maptype=roadmap&amp;sensor=false"
+          >' . $job_location . '</a
         >
       </td>
       <td class="job_status column-job_status" data-colname="ステータス">
-        <span data-tip="有効" class="tips status-publish">有効</span>
+        ' . $status_icon . '
       </td>
       <td class="job_posted column-job_posted" data-colname="掲載中">
-        <strong>2022年2月22日</strong
+        <strong>' . $post_date . '</strong
         ><span
           >投稿者:
           <a
@@ -72,7 +80,7 @@ function make_job_openings_table_row($title) {
         >
       </td>
       <td class="job_expires column-job_expires" data-colname="期限">
-        <strong>2022年5月23日</strong>
+        <strong>' . $job_expires . '</strong>
       </td>
       <td
         class="job_listing_category column-job_listing_category"
@@ -86,29 +94,31 @@ function make_job_openings_table_row($title) {
         <div class="actions">
           <a
             class="button button-icon tips icon-view"
-            href="http://aisohitoshi.wp.xdomain.jp/job/%e3%82%a2%e3%83%b3%e3%82%bf%e3%83%83%e3%83%81%e3%83%a3%e3%83%96%e3%83%ab-%e5%9c%b0%e7%90%83-full-time-%e3%82%b7%e3%83%a3%e3%83%83%e3%83%81%e3%83%a7%e3%81%95%e3%82%93/"
+            href="http://aisohitoshi.wp.xdomain.jp/job/%e3%82%a2%e3%83%b3%e3%82%bf%e3%83%83%e3%83%81%e3%83%a3%e3%83%96%e3%83%ab-%e5%9c%b0%e7%90%83-full-time-%e3%82%b7%e3%83%a3%e3%83%83%e3%83%81%e3%83%a7%e3%81%95%e3%82%93/(TODO)"
             data-tip="表示"
             >表示</a
           ><a
             class="button button-icon tips icon-edit"
-            href="http://aisohitoshi.wp.xdomain.jp/wp-admin/post.php?post=11&amp;action=edit"
+            href="' . $admin_url . 'post.php?post=' . $post_id . '&amp;action=edit"
             data-tip="編集"
             >編集</a
           ><a
             class="button button-icon tips icon-delete"
-            href="http://aisohitoshi.wp.xdomain.jp/wp-admin/post.php?post=11&amp;action=trash&amp;_wpnonce=cf3bb74013"
+            href="' . $admin_url . 'post.php?post=' . $post_id . '&amp;action=trash&amp;_wpnonce=cf3bb74013"
             data-tip="削除"
             >削除</a
           >
         </div>
       </td>
     </tr>
-';
+  ';
   return $job_openings_table_main;
 }
 
 
-function make_job_openings_table_head() {
+function make_job_openings_table_head()
+{
+  $admin_url = esc_url(get_admin_url(''));
   echo '
   <table class="wp-list-table widefat fixed striped table-view-list posts">
   <thead>
@@ -124,8 +134,8 @@ function make_job_openings_table_head() {
         class="manage-column column-job_position column-primary sorted desc"
       >
         <a
-          href="http://aisohitoshi.wp.xdomain.jp/wp-admin/edit.php?post_type=job_listing&amp;orderby=title&amp;order=asc"
-          ><span>位置</span><span class="sorting-indicator"></span
+          href="' . $admin_url . 'edit.php?post_type=job_listing&amp;orderby=title&amp;order=asc"
+          ><span>ポジション</span><span class="sorting-indicator"></span
         ></a>
       </th>
       <th
@@ -141,7 +151,7 @@ function make_job_openings_table_head() {
         class="manage-column column-job_location sortable desc"
       >
         <a
-          href="http://aisohitoshi.wp.xdomain.jp/wp-admin/edit.php?post_type=job_listing&amp;orderby=job_location&amp;order=asc"
+          href="' . $admin_url . 'edit.php?post_type=job_listing&amp;orderby=job_location&amp;order=asc"
           ><span>所在地</span><span class="sorting-indicator"></span
         ></a>
       </th>
@@ -154,7 +164,7 @@ function make_job_openings_table_head() {
         class="manage-column column-job_posted sortable desc"
       >
         <a
-          href="http://aisohitoshi.wp.xdomain.jp/wp-admin/edit.php?post_type=job_listing&amp;orderby=date&amp;order=asc"
+          href="' . $admin_url . 'edit.php?post_type=job_listing&amp;orderby=date&amp;order=asc"
           ><span>掲載中</span><span class="sorting-indicator"></span
         ></a>
       </th>
@@ -164,7 +174,7 @@ function make_job_openings_table_head() {
         class="manage-column column-job_expires sortable desc"
       >
         <a
-          href="http://aisohitoshi.wp.xdomain.jp/wp-admin/edit.php?post_type=job_listing&amp;orderby=job_expires&amp;order=asc"
+          href="' . $admin_url . 'edit.php?post_type=job_listing&amp;orderby=job_expires&amp;order=asc"
           ><span>期限</span><span class="sorting-indicator"></span
         ></a>
       </th>
@@ -193,7 +203,10 @@ function make_job_openings_table_head() {
   ';
 }
 
-function make_job_openings_table_foot() {
+function make_job_openings_table_foot()
+{
+  $admin_url = esc_url(get_admin_url(''));
+
   echo '
   <tfoot>
       <tr>
@@ -207,14 +220,14 @@ function make_job_openings_table_foot() {
           class="manage-column column-job_position column-primary sorted desc"
         >
           <a
-            href="http://aisohitoshi.wp.xdomain.jp/wp-admin/edit.php?post_type=job_listing&amp;orderby=title&amp;order=asc"
-            ><span>位置</span><span class="sorting-indicator"></span
+            href="' . $admin_url . 'edit.php?post_type=job_listing&amp;orderby=title&amp;order=asc"
+            ><span>ポジション</span><span class="sorting-indicator"></span
           ></a>
         </th>
         <th scope="col" class="manage-column column-job_listing_type">タイプ</th>
         <th scope="col" class="manage-column column-job_location sortable desc">
           <a
-            href="http://aisohitoshi.wp.xdomain.jp/wp-admin/edit.php?post_type=job_listing&amp;orderby=job_location&amp;order=asc"
+            href="' . $admin_url . 'edit.php?post_type=job_listing&amp;orderby=job_location&amp;order=asc"
             ><span>所在地</span><span class="sorting-indicator"></span
           ></a>
         </th>
@@ -223,13 +236,13 @@ function make_job_openings_table_foot() {
         </th>
         <th scope="col" class="manage-column column-job_posted sortable desc">
           <a
-            href="http://aisohitoshi.wp.xdomain.jp/wp-admin/edit.php?post_type=job_listing&amp;orderby=date&amp;order=asc"
+            href="' . $admin_url . 'edit.php?post_type=job_listing&amp;orderby=date&amp;order=asc"
             ><span>掲載中</span><span class="sorting-indicator"></span
           ></a>
         </th>
         <th scope="col" class="manage-column column-job_expires sortable desc">
           <a
-            href="http://aisohitoshi.wp.xdomain.jp/wp-admin/edit.php?post_type=job_listing&amp;orderby=job_expires&amp;order=asc"
+            href="' . $admin_url . 'edit.php?post_type=job_listing&amp;orderby=job_expires&amp;order=asc"
             ><span>期限</span><span class="sorting-indicator"></span
           ></a>
         </th>
--- job_opening_plugin/view/view.php
+++ job_opening_plugin/view/view.php
@@ -9,7 +9,8 @@ include_once(JOB_OPENING__PLUGIN_DIR . 'view/template/addCompany.php');
  * (未使用) メインメニューページ内容の表示・更新処理
  * @deprecated version 0.1
  */
-function main_menu_page_contents() {
+function main_menu_page_contents()
+{
   // HTML表示
   echo <<<EOF
 <div class="wrap">
@@ -23,9 +24,71 @@ EOF;
 
 
 //=================================================
-// サブメニュー①ページ内容の表示・更新処理
+// サブメニュー  ページ内容の表示・更新処理
 //=================================================
-function job_openings_list() {
+/**
+ * 求人一覧ページ用の関数
+ */
+function job_openings_list()
+{
+  $args = array(
+    'post_type' => 'post',  // 投稿タイプ
+    // 'category_name' => 'カテゴリのスラッグ',	// 絞り込むカテゴリ
+    // 'tag' => 'タグスラッグ',	// 絞り込むタグ
+    // 's' => '検索文字列',	// 検索文字列
+    // 'posts_per_page' => 3,	// 表示件数
+    'post_status' => 'publish, inherit, pending, private, future, draft, trash',  // 全て取得
+    'post_type' => 'job_listing',
+    'orderby' => 'date',  //新着順
+    'order' => 'ASC',  // 昇順
+  );
+
+  //ここから
+
+  //ここまで
+
+  $the_query = get_posts($args);
+  // query_posts($args);
+  // $posts_array = get_posts($args);
+
+  echo make_job_openings_table_head();
+  echo '<tbody id="the-list">';
+  // ループ
+  global $post;
+  foreach ($the_query as $post) :
+    setup_postdata($post);
+    $post_id = get_the_ID();
+    $title = get_the_title();
+    $author = get_the_author();
+    $post_date = get_the_date();
+    $job_expires = get_post_meta($post_id, '_job_expires', true);
+    $job_location = get_post_meta($post_id, '_job_location', true);
+
+    $status_icon = "";
+    /**
+     * To check kinds of status,
+     * see: https://wpdocs.osdn.jp/%E9%96%A2%E6%95%B0%E3%83%AA%E3%83%95%E3%82%A1%E3%83%AC%E3%83%B3%E3%82%B9/get_post_status
+     * TODO: if you have to separate more kinds, use switch statement.
+     */
+    if (get_post_status($post_id) == 'publish') {
+      $status_icon = '<span data-tip="公開中" class="tips status-publish">公開中</span>';
+    } else {
+      $status_icon = '<span data-tip="非公開" class="tips status-draft">非公開</span>';
+    }
+    echo make_job_openings_table_row($post_id, $title, $author, $post_date, $job_expires, $job_location, $status_icon);
+  endforeach;
+  echo '</tbody>';
+
+  wp_reset_postdata(); // 投稿データのリセット
+  // 表 フッターの表示
+  echo make_job_openings_table_foot();
+}
+
+/**
+ * 企業一覧ページ用の関数
+ */
+function company_list()
+{
   $args = array(
     'post_type' => 'post',  // 投稿タイプ
     // 'category_name' => 'カテゴリのスラッグ',	// 絞り込むカテゴリ
@@ -47,7 +110,7 @@ function job_openings_list() {
     echo '<tbody id="the-list">';
     while (have_posts()) :
       the_post();
-      echo make_job_openings_table_row(the_title());
+    // echo make_job_openings_table_row(the_title());
     endwhile;
     echo '</tbody>';
   endif;
@@ -56,50 +119,25 @@ function job_openings_list() {
   wp_reset_query();
 }
 
-function company_list() {
-  $args = array(
-    'post_type' => 'post',  // 投稿タイプ
-    // 'category_name' => 'カテゴリのスラッグ',	// 絞り込むカテゴリ
-    // 'tag' => 'タグスラッグ',	// 絞り込むタグ
-    // 's' => '検索文字列',	// 検索文字列
-    // 'posts_per_page' => 3,	// 表示件数
-    // 'post_status' => 'publish',	// 公開済みのみ
-    'post_type' => 'job_listing',
-    'orderby' => 'date',  //新着順
-    'order' => 'ASC',  // 昇順
-  );
-
-  query_posts($args);
-  // $posts_array = get_posts($args);
-
-  echo make_job_openings_table_head();
-  // ループ
-  if (have_posts()) :
-    echo '<tbody id="the-list">';
-    while (have_posts()) :
-      the_post();
-      echo make_job_openings_table_row(the_title());
-    endwhile;
-    echo '</tbody>';
-  endif;
-  echo make_job_openings_table_foot();
-  // 投稿データのリセット
-  wp_reset_query();
-}
-
-function job_openings_add() {
+/**
+ * 求人追加ページ用の関数
+ */
+function job_openings_add()
+{
   echo create_job_opening();
 }
 
-function company_add() {
+/**
+ * 企業追加ページ用の関数
+ */
+function company_add()
+{
   echo create_company();
 }
 
 
-//=================================================
-// サブメニュー②ページ内容の表示・更新処理
-//=================================================
-function job_openings_TODO() {
+function job_openings_TODO()
+{
 
   //---------------------------------
   // ユーザーが必要な権限を持つか確認
