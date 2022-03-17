77c043aaa04b8516aeed860ff1cd515408736265
yui666a <haiso666@gmail.com>
2022-03-17 21:36:33 +0900

    ヘッダーcssの修正

--- job_opening_plugin/view/template/header.php
+++ job_opening_plugin/view/template/header.php
@@ -5,22 +5,22 @@ function qqqq()
   $html = '';
   $html = <<<EOF
   <div class="step-wrapper">
-    <div class="step-number" id="step1-index">1</div>
+    <!-- <div class="step-number" id="step1-index">1</div> -->
     <div class="step-text" id="step1-label">
       <a href="http://">企業情報を新規追加</a>
     </div>
 
-    <div class="step-number" id="step2-index">2</div>
+    <!-- <div class="step-number" id="step2-index">2</div> -->
     <div class="step-text" id="step2-label">
       <a href="http://">作成した企業一覧</a>
     </div>
 
-    <div class="step-number" id="step3-index">3</div>
+    <!-- <div class="step-number" id="step3-index">3</div> -->
     <div class="step-text" id="step3-label">
       <a href="http://">求人情報を新規追加</a>
     </div>
 
-    <div class="step-number" id="step4-index">4</div>
+    <!-- <div class="step-number" id="step4-index">4</div> -->
     <div class="step-text" id="step4-label">
       <a href="http://">作成した求人一覧</a>
     </div>
