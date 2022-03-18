4c6c5ba9ae28699966298250b27970b7c29a8745
yui666a <haiso666@gmail.com>
2022-03-18 09:09:36 +0900

    tinymce 起動コードの修正

--- job_opening_plugin/js/tinymce.js
+++ job_opening_plugin/js/tinymce.js
@@ -13,3 +13,23 @@ $(function () {
   });
   console.log("tinymce.init ends");
 });
+
+$('textarea.rich').tinymce({
+  selector: "textarea.rich", // <textarea class="rich"...></textarea>
+  forced_root_block: "div", // modify default tag <p> -> <div>
+  language: "ja",
+  branding: false, // remove credit
+  setup: function (editor) {
+    editor.on("change", function () {
+      tinymce.triggerSave();
+    });
+  },
+  height: 300,
+  // menubar: false,
+  // plugins: [
+  //   'advlist autolink lists link image charmap print preview anchor',
+  //   'searchreplace visualblocks code fullscreen',
+  //   'insertdatetime media table paste code help wordcount'
+  // ],
+  // toolbar: 'undo redo | formatselect | bold italic backcolor | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | removeformat | help'
+});
