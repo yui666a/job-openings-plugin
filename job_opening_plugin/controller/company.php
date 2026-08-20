7376ebb57ed9665b2b1de89e2aa79df4f1d50dca
AISO, Hitoshi <haiso666@gmail.com>
2026-08-21 00:29:34 +0900

    fix: 企業情報のクエリを prepare 経由にして SQL インジェクションを塞ぐ (#58)
    * fix: 企業情報のクエリを prepare 経由にして SQL インジェクションを塞ぐ
    
    $_GET["id"] が検証されないまま文字列連結で SQL に組み込まれており、
    contributor 以上の権限があれば UNION 注入で wp_users のパスワードハッシュを
    読み出せる状態だった。所有者チェックは SQL 実行より後に行われるため防御に
    なっていない。?action=remove&id=1 OR 1=1 による企業情報の全削除も成立する。
    
    企業情報を扱う全クエリを $wpdb->prepare() のプレースホルダ経由に変更する。
    あわせて view.php の受け取り時点で absint() を通し、数値以外が SQL に到達
    しないようにした。企業が引けなかった場合に $company->user_id を参照して
    いた箇所も null チェックを加えている。
    
    DELETE 文は結果を取得する必要がないため get_results() から query() に変更した。
    
    Refs #42
    
    * fix: 企業が引けないときの未定義オフセット警告を防ぐ
    
    getCompanyById() は該当レコードがないと $companies[0] で未定義オフセットの
    警告を出したうえで null を返していた。企業を削除した直後は、その企業を参照
    する求人が残るため card.php / jobTable.php / userJobOpening.php から確実に
    踏む。呼び出し側で null を判定できるよう、警告を出さずに null を返す。
    
    Refs #42

--- job_opening_plugin/controller/company.php
+++ job_opening_plugin/controller/company.php
@@ -11,7 +11,10 @@ function getCompanies()
 function getCompaniesByUserId($userId)
 {
   global $wpdb;
-  $query = "SELECT * FROM `" . $wpdb->prefix . "sac_job_opening_companies` WHERE user_id=" . $userId . ";";
+  $query = $wpdb->prepare(
+    "SELECT * FROM `{$wpdb->prefix}sac_job_opening_companies` WHERE user_id = %d",
+    $userId
+  );
   $companies = $wpdb->get_results($query, OBJECT);
   return $companies;
 }
@@ -19,14 +22,21 @@ function getCompaniesByUserId($userId)
 function getCompanyById($companyId)
 {
   global $wpdb;
-  $query = "SELECT * FROM `" . $wpdb->prefix . "sac_job_opening_companies`WHERE co_id=" . $companyId . ";";
+  $query = $wpdb->prepare(
+    "SELECT * FROM `{$wpdb->prefix}sac_job_opening_companies` WHERE co_id = %d",
+    $companyId
+  );
   $companies = $wpdb->get_results($query, OBJECT);
-  return $companies[0];
+  // 該当なしのとき $companies[0] は未定義オフセットの警告を出すため、null に丸める
+  return $companies[0] ?? null;
 }
 
 function deleteCompaniesByCompanyId($companyId)
 {
   global $wpdb;
-  $query = "DELETE FROM `" . $wpdb->prefix . "sac_job_opening_companies` WHERE " . $wpdb->prefix . "sac_job_opening_companies.co_id = " . $companyId . ";";
-  $wpdb->get_results($query, OBJECT);
+  $query = $wpdb->prepare(
+    "DELETE FROM `{$wpdb->prefix}sac_job_opening_companies` WHERE co_id = %d",
+    $companyId
+  );
+  $wpdb->query($query);
 }
